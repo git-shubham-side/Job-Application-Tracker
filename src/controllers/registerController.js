7dbc0d1526763736d7a0fa05232874d5e7d79d49
Shubh <rathodshubham7711@gmail.com>
2026-07-08 01:55:25 +0530

    session added

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -7,27 +7,32 @@ const errorhandler = require("../middlewares/errorHandler");
 
 const registerPostController = async function (req, res, next) {
   const { firstName, lastName, email, terms, password } = req.body;
+
   try {
-    const hashedPassword = await bcrypt.hash(password, 10);
+    if ((firstName, lastName, email, terms, password)) {
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-    // User Regestraion Using Signup From
-    // Post : /api/v1/signup
+      // User Regestraion Using Signup From
+      // Post : /api/v1/signup
 
-    const user = new User({
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      terms: terms === "on",
-      password: hashedPassword,
-    });
-    user.save();
+      const user = new User({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        terms: terms === "on",
+        password: hashedPassword,
+      });
+      // Uncomment Later
+      // user.save();
+      res.status(201).json({ message: "accont created successfully!" });
 
-    //Testing Error Handler Middleware
-    // throw new Error("Something went wrong!");
-
-    // Redirection Link : /api/v1/dashboard
-    // When user successfully creates an account and redirect to Dashboard
-    res.redirect("/api/v1/dashboard", { user: user });
+      // Redirection Link : /api/v1/dashboard
+      // When user successfully creates an account and redirect to Dashboard
+      res.redirect("/api/v1/dashboard");
+    } else {
+      //If the Users does not sends the data properly
+      res.status(401).json({ message: "Please enter the data in all fields" });
+    }
   } catch (err) {
     // res.status(500).json({
     //   message: err.message,
