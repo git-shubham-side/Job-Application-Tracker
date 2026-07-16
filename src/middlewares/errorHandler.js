4ccadf6ace50e8665dbc77740b256e1bed4b40be
Shubh <rathodshubham7711@gmail.com>
2026-07-16 15:06:49 +0530

    errorHandler Updated

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,8 +1,5 @@
 const errorHandler = async function (err, req, res, next) {
-  console.log(err);
   res.render("Server_Error/internel_server_error", { err });
 };
 
 module.exports = errorHandler;
-
-//Implement flash message in the error handler
